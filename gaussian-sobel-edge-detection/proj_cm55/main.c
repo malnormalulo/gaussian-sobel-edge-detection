cd745de8e6627205e7bc0005de607883827f090e
Malnormalulo <malnormalulo1@gmail.com>
2026-04-09 22:56:41 +0200

    integer gaussian_kernel

--- gaussian-sobel-edge-detection/proj_cm55/main.c
+++ gaussian-sobel-edge-detection/proj_cm55/main.c
@@ -12,8 +12,6 @@
 // #include "core_naive.h"
 #include "core.h"
 
-#include "utills.h"
-
 
 void print_summary(const char * fname, 
     const uint8_t * actual, 
@@ -62,37 +60,37 @@ CY_SECTION(".cy_itcm") int main(void)
 
     init_retarget_io();
     perf_counter_init();
-    printf("Init finished\n");
-    printf("SystemCoreClock: %lu Hz (%.2f MHz)\n\n", SystemCoreClock, SystemCoreClock / 1000000.0);
+    // printf("Init finished\n");
+    // printf("SystemCoreClock: %lu Hz (%.2f MHz)\n\n", SystemCoreClock, SystemCoreClock / 1000000.0);
 
     size_t size = IN_HEIGHT * IN_WIDTH;
 
     fill_gaussian_blur_kernel();
-    print_2D_float_array(GBLUR_KERNEL_SIZE, GBLUR_KERNEL_SIZE, gaussian_kernel);
+    print_gaussian_kernel();
 
-    mac = size * 3;
-    uint8_t *actual_out_monochrome = malloc(size * sizeof(uint8_t));
-    perf_counter_start();
-    convert_to_monochrome(size, input_image, actual_out_monochrome);
-    res = perf_counter_stop();
-    print_summary("\nmonochrome", actual_out_monochrome, out_monochrome, size, mac, res);
+    // mac = size * 3;
+    // uint8_t *actual_out_monochrome = malloc(size * sizeof(uint8_t));
+    // perf_counter_start();
+    // convert_to_monochrome(size, input_image, actual_out_monochrome);
+    // res = perf_counter_stop();
+    // print_summary("\nmonochrome", actual_out_monochrome, out_monochrome, size, mac, res);
 
-    mac = GBLUR_KERNEL_SIZE*GBLUR_KERNEL_SIZE*size;
-    uint8_t *actual_out_gaussian_blur = malloc(size * sizeof(uint8_t));
-    perf_counter_start();
-    gaussian_blur(IN_HEIGHT, IN_WIDTH, actual_out_monochrome, actual_out_gaussian_blur);
-    res = perf_counter_stop();
-    print_summary("gaussian blur", actual_out_gaussian_blur, out_gaussian_blur, size, mac, res);
-    free(actual_out_monochrome);
+    // mac = GBLUR_KERNEL_SIZE*GBLUR_KERNEL_SIZE*size;
+    // uint8_t *actual_out_gaussian_blur = malloc(size * sizeof(uint8_t));
+    // perf_counter_start();
+    // gaussian_blur(IN_HEIGHT, IN_WIDTH, actual_out_monochrome, actual_out_gaussian_blur, GBLUR_KERNEL_SIZE, gaussian_kernel);
+    // res = perf_counter_stop();
+    // print_summary("gaussian blur", actual_out_gaussian_blur, out_gaussian_blur, size, mac, res);
+    // free(actual_out_monochrome);
 
-    mac = SED_KERNEL_SIZE*SED_KERNEL_SIZE*size;
-    uint8_t *actual_out_sobel = malloc(size * sizeof(uint8_t));
-    perf_counter_start();
-    sobel_edge_detection(IN_HEIGHT, IN_WIDTH, actual_out_gaussian_blur, actual_out_sobel);
-    res = perf_counter_stop();
-    print_summary("sobel edge", actual_out_sobel, out_sobel_edge, size, mac, res);
-    free(actual_out_monochrome);
-    free(actual_out_sobel);
+    // mac = SED_KERNEL_SIZE*SED_KERNEL_SIZE*size;
+    // uint8_t *actual_out_sobel = malloc(size * sizeof(uint8_t));
+    // perf_counter_start();
+    // sobel_edge_detection(IN_HEIGHT, IN_WIDTH, actual_out_gaussian_blur, actual_out_sobel);
+    // res = perf_counter_stop();
+    // print_summary("sobel edge", actual_out_sobel, out_sobel_edge, size, mac, res);
+    // free(actual_out_monochrome);
+    // free(actual_out_sobel);
 
     // mac = size*3 + GBLUR_KERNEL_SIZE*GBLUR_KERNEL_SIZE*size + SED_KERNEL_SIZE*SED_KERNEL_SIZE*size;
     // uint8_t *actual_out_monochrome = malloc(size * sizeof(uint8_t));
