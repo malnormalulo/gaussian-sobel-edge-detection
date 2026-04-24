898337b80215db3e7916032f62125263232edda1
Malnormalulo <malnormalulo1@gmail.com>
2026-04-24 16:14:00 +0200

    buffer allocation for 2 test case

--- gaussian-sobel-edge-detection/proj_cm55/main.c
+++ gaussian-sobel-edge-detection/proj_cm55/main.c
@@ -97,11 +97,12 @@ CY_SECTION(".cy_itcm") int main(void)
     // mac = size*3 + GBLUR_KERNEL_SIZE*GBLUR_KERNEL_SIZE*size + SED_KERNEL_SIZE*SED_KERNEL_SIZE*size;
     // uint8_t *actual_out_monochrome = malloc(size * sizeof(uint8_t));
     // uint8_t *actual_out_gaussian_blur = malloc(size * sizeof(uint8_t));
+    // uint8_t *buffer = malloc(size * sizeof(uint8_t));
     // uint8_t *actual_out_sobel = malloc(size * sizeof(uint8_t));
 
     // perf_counter_start();
     // convert_to_monochrome(size, input_image, actual_out_monochrome);
-    // gaussian_blur(IN_HEIGHT, IN_WIDTH, actual_out_monochrome, actual_out_gaussian_blur);
+    // gaussian_blur(IN_HEIGHT, IN_WIDTH, actual_out_monochrome, actual_out_gaussian_blur, buffer);
     // sobel_edge_detection(IN_HEIGHT, IN_WIDTH, actual_out_gaussian_blur, actual_out_sobel);
     // res = perf_counter_stop();
 
